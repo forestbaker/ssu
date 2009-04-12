96ec2e71b95e6d65fe3f502a61510bc560995b8a
kikainekox <kikainekox@c8fe07f4-df78-11dd-84fc-932898d6254d>
2009-04-12 01:05:27 +0000

    no japanese

--- ssu_java/src/org/kikaineko/ssu/Analyzer.java
+++ ssu_java/src/org/kikaineko/ssu/Analyzer.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.kikaineko.ssu;
 
 import java.util.ArrayList;
@@ -5,11 +18,17 @@ import java.util.Arrays;
 
 import org.kikaineko.util.FileIO;
 
+/**
+ * 
+ * @author Masayuki Ioki
+ *
+ */
 public class Analyzer {
 
-	public static void analyze(String expectedFile, String resultFile,String name)
-			throws Exception {
-		ArrayList resultlines = cleaned(FileIO.getFileDatas(resultFile,FileIO.FileReadCode));
+	public static void analyze(String expectedFile, String resultFile,
+			String name) throws Exception {
+		ArrayList resultlines = cleaned(FileIO.getFileDatas(resultFile,
+				FileIO.FileReadCode));
 		ArrayList tempResult = new ArrayList();
 		for (int i = 0; i < resultlines.size(); i++) {
 			String s = (String) resultlines.get(i);
@@ -22,10 +41,9 @@ public class Analyzer {
 		for (int i = 0; i < tempResult.size(); i++) {
 			results[i] = Integer.parseInt((String) tempResult.get(i));
 		}
-		ArrayList expectedlines = cleaned(FileIO.getFileDatas(expectedFile,FileIO.FileReadCode));
-		if(expectedlines.size()==0){
-			//なんらかの理由で期待する行が取得されていない
-			//これはカバレッジ取得対象ではない
+		ArrayList expectedlines = cleaned(FileIO.getFileDatas(expectedFile,
+				FileIO.FileReadCode));
+		if (expectedlines.size() == 0) {
 			return;
 		}
 		int[] expecteds = new int[expectedlines.size()];
@@ -43,7 +61,7 @@ public class Analyzer {
 			}
 		}
 		float doneLines = expecteds.length - cnt;
-		result(sb, doneLines, expectedlines.size(),name);
+		result(sb, doneLines, expectedlines.size(), name);
 	}
 
 	private static ArrayList cleaned(ArrayList fileDatas) {
@@ -57,7 +75,8 @@ public class Analyzer {
 		return tmp;
 	}
 
-	protected static void result(StringBuffer sb, float doneLines, int max,String name) {
+	protected static void result(StringBuffer sb, float doneLines, int max,
+			String name) {
 		int rate = (int) (((doneLines) / max) * 100);
 		System.out.println("==Coverage Report.==========================");
 		if (sb.length() > 0) {
--- ssu_java/src/org/kikaineko/ssu/Report.java
+++ ssu_java/src/org/kikaineko/ssu/Report.java
@@ -1,5 +1,22 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.kikaineko.ssu;
-
+/**
+ * 
+ * @author Masayuki Ioki
+ *
+ */
 public class Report {
 	private static final String RED = "\\033[41m\\033[1;37m";
 	private static final String GREEN = "\\033[42m\\033[1;37m";
